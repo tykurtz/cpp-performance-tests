08d973d879bc74cd2a7098126bc76e7250b9912e
Tyler Kurtz <tylerkurtz8@gmail.com>
2018-12-05 12:05:41 -0500

    Tweaked array size a bit

--- x-value-optimization/src/push_back.cpp
+++ x-value-optimization/src/push_back.cpp
@@ -5,13 +5,13 @@
 
 #include <assert.h>
 
-typedef std::array<int, 100> BigArray;
+typedef std::array<int, 10000> BigArray;
 
 int main()
 {
   std::vector<BigArray> big_array_vector;
-  const int VECTOR_COUNT = 10000000;
-  //big_array_vector.reserve(VECTOR_COUNT);
+  const int VECTOR_COUNT = 10000;
+  big_array_vector.reserve(VECTOR_COUNT);
 
   for (int i = 0; i < VECTOR_COUNT; i++)
   {
